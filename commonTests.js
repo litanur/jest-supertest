89d17ba21fce62644c68ffb9cef93132e93ff1dd
litanur <litanurlaelati@gmail.com>
2021-08-04 11:14:10 +0700

    add test files

---
+++ commonTests.js
@@ -0,0 +1,18 @@
+var supertest = require('supertest'); //require supertest
+const request = supertest('https://reqres.in/'); //supertest hits the HTTP server (your app)
+
+/*
+This piece of code is for getting the authorization token after login to your app.
+const token;
+test("Login to the application", function(){
+    return request.post(``).then((response)=>{
+        token = response.body.token  //to save the login token for further requests
+    })
+}); 
+*/
+
+module.exports = 
+{
+    request
+        //, token     -- export if token is generated
+}
